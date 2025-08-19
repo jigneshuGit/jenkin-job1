a951d3f60f0780b22beb3bba100765e9d9d07797
jigneshuGit <123591992+jigneshuGit@users.noreply.github.com>
2025-08-20 00:33:08 +0530

    Update jenkinjob1.java

--- jenkinjob1.java
+++ jenkinjob1.java
@@ -1,5 +1,5 @@
 public class jenkinjob1 {
     public static void main(String[] args) {
-        System.out.println("Hello from Jenkins Job 1");
+        System.out.println("Hello from new Jenkins Job 1 to test automation of CI");
     }
 }
