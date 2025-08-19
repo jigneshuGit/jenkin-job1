a2cf2515b3e1d81ebb2b36d0dc5d891b5aba970b
jigneshuGit <123591992+jigneshuGit@users.noreply.github.com>
2025-08-19 21:09:18 +0530

    Update jenkin.java

--- jenkin.java
+++ jenkin.java
@@ -1,5 +1,5 @@
 public class jenkin {
     public static void main(String[] args) {
-        System.out.println("Welcome to Jenkins Practical");
+        System.out.println("Welcome to Auto cron job in Jenkins Practical");
     }
 }
